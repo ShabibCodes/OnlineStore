c37e3e5d2dcf9a865546a72d40d3dd520b55d83d
Shabib Mohammed <uniqueb7h@gmail.com>
2023-01-26 15:18:20 +0300

    add env

---
+++ OnlineStore/src/environments/environment.development.ts
@@ -0,0 +1,21 @@
+export const environment = {
+
+    POSTGRES_HOST:'127.0.0.1',
+
+POSTGRES_DB: 'onlinestore',
+
+POSTGRES_TEST_DB: 'onlinestoretest',
+
+POSTGRES_USER: 'admin',
+
+POSTGRES_PASSWORD: '123',
+
+TOKEN_SIGN: 'secret',
+
+BCRYPT: 'strongSecret',
+
+SALT_ROUNDS:5,
+
+ENV: 'DEV'
+
+};
